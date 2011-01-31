b1de781aa9928d5507480313034507f4be8c93fd
Nick Bruun <mail@nickbruun.dk>
2011-01-31 12:25:06 +0100

    Cleanup before version 1.1-pre

--- src/Twentythree/bin/deploy/Twentythree/deploywithstsadmnocontenturls.bat
+++
@@ -1,8 +0,0 @@
-"C:\Program Files\Common Files\Microsoft Shared\web server extensions\12\BIN\stsadm.exe" -o retractsolution -name "Twentythree.wsp" -immediate
-"C:\Program Files\Common Files\Microsoft Shared\web server extensions\12\BIN\stsadm.exe" -o execadmsvcjobs
-"C:\Program Files\Common Files\Microsoft Shared\web server extensions\12\BIN\stsadm.exe" -o deletesolution -name "Twentythree.wsp"
-"C:\Program Files\Common Files\Microsoft Shared\web server extensions\12\BIN\stsadm.exe" -o execadmsvcjobs
-"C:\Program Files\Common Files\Microsoft Shared\web server extensions\12\BIN\stsadm.exe" -o addsolution -filename "Twentythree.wsp"
-"C:\Program Files\Common Files\Microsoft Shared\web server extensions\12\BIN\stsadm.exe" -o execadmsvcjobs
-"C:\Program Files\Common Files\Microsoft Shared\web server extensions\12\BIN\stsadm.exe" -o deploysolution -name "Twentythree.wsp" -immediate -allowGacDeployment -allowCasPolicies -force
-"C:\Program Files\Common Files\Microsoft Shared\web server extensions\12\BIN\stsadm.exe" -o execadmsvcjobs
